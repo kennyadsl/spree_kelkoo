7a3175313914292bd7e251e3768d1d42a85c348b
Alberto Vena <kennyadsl@gmail.com>
2013-09-02 18:44:36 +0200

    uses memoization to allow products overloading

--- app/controllers/products_decorator.rb
+++ app/controllers/products_decorator.rb
@@ -2,11 +2,11 @@ ProductsController.class_eval do
   caches_action :kelkoo, :pipe, :expires_in => 1.hour
 
   def kelkoo
-    @products = Product.active
+    @products ||= Product.active
   end
   
   def pipe
-    @products = Product.active
+    @products ||= Product.active
     render :layout => false 
   end
 end
