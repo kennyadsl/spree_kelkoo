d4a70ad998b13b5507f3bf6fefd45f05b2a9cb2d
Alberto Vena <kennyadsl@gmail.com>
2013-09-12 10:42:49 +0200

    adds unapproved products filter

--- app/controllers/products_decorator.rb
+++ app/controllers/products_decorator.rb
@@ -1,12 +1,17 @@
 ProductsController.class_eval do
+  before_filter :feeds_filter, :only => [:kelkoo, :pipe]
   caches_action :kelkoo, :pipe, :expires_in => 1.hour
 
   def kelkoo
-    @products ||= Product.active
   end
-  
+
   def pipe
-    @products ||= Product.active
-    render :layout => false 
+    render :layout => false
+  end
+
+  private
+
+  def feeds_filter
+    @products = Product.active.where("UPPER(name) NOT LIKE UPPER(?) AND UPPER(name) NOT LIKE UPPER(?)", "%sigaretta%", "%nicotina%")
   end
 end
