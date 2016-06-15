fab7c75563f734cf65d1857c27427d3a8e0143d1
Pierpaolo Pantone <24alsecondo@gmail.com>
2016-06-15 12:38:10 +0200

    removes unused and potentially dangerous routes
    Removing generic products routes is also necessary for internationalization purposes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  resources :products do 
-    collection do 
+  resources :products, only: [] do
+    collection do
       get :kelkoo
     end
   end
-  
+
   namespace :admin do
     resource :kelkoo
   end
